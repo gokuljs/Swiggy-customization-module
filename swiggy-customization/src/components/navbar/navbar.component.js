0aa818db39cf4b691a081a73d30462874f4ba3ed
gokuljs <gokuljs029@gmail.com>
2021-11-03 22:09:14 +0530

    navbar

--- swiggy-customization/src/components/navbar/navbar.component.js
+++ swiggy-customization/src/components/navbar/navbar.component.js
@@ -1,8 +1,27 @@
 import React from 'react';
-import './navbar.styles.scss'
+import swiggy from './swiggy.png';
+import offer from './offers.png';
+import signin from './sign.png';
+
+import './navbar.styles.scss';
 
 const NavBar=()=>(
-    <div>getting started with building a navabr section</div>
+    <div className="navbar">
+        <div className="container">
+        <img src={swiggy} className="logo"/>
+        <nav>
+        <ul>
+            <li><a href="#"><i class="fas fa-search"></i>Search</a></li>
+            <li><a href="#"><img src={offer} className="offer"/>Offers</a></li>
+            <li><a href="#"><i class="far fa-life-ring"></i>Help</a></li>
+            <li><a href="#"><img src={signin} className="signin"/>Sign In</a></li>
+            <li><a href="#">Cart</a></li>
+        </ul>
+        </nav>
+
+        </div>
+        
+    </div>
 )
 
 export default NavBar;
