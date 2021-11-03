2d8297c71505a370d26d8997dd56bd25f389307c
gokuljs <gokuljs029@gmail.com>
2021-11-03 19:22:10 +0530

    getting started with navbar section

--- swiggy-customization/src/App.js
+++ swiggy-customization/src/App.js
@@ -1,12 +1,17 @@
 import React,{Component} from 'react';
+import NavBar from './components/navbar/navbar.component';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      hello world
-    </div>
-  );
+class App extends Component{
+  render(){
+    return(
+      <div className="app">	
+          <NavBar />
+      </div>
+      
+
+    )
+  }
 }
 
 export default App;
