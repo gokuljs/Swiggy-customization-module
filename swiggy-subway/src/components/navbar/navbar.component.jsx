615f3771d37e9481541fca13a20cd8f2751c615e
gokuljs <gokuljs029@gmail.com>
2021-11-04 07:46:06 +0530

    navabr.png

--- swiggy-subway/src/components/navbar/navbar.component.jsx
+++ swiggy-subway/src/components/navbar/navbar.component.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import swiggy from './swiggy.png'
 import './navbar.styles.scss';
  const NavBar=()=>(
-     <div>hello world</div>
+    <header className="header">
+    <nav className="navbar">
+        <a href="#" className="nav-logo"><img className="brand" src={swiggy}/></a>
+        <ul className="nav-menu">
+            <li className="nav-item">
+                <a href="#" className="nav-link">Search</a>
+            </li>
+            <li className="nav-item">
+                <a href="#" className="nav-link">offers</a>
+            </li>
+            <li className="nav-item">
+                <a href="#" className="nav-link">Help</a>
+            </li>
+            <li className="nav-item">
+                <a href="#" className="nav-link">Sign In</a>
+            </li>
+            <li className="nav-item">
+                <a href="#" className="nav-link">Cart</a>
+            </li>
+        </ul>
+        <div className="hamburger">
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+        </div>
+    </nav>
+</header>
+
  )
 
  export default NavBar;
