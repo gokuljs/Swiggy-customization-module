98f686488a64c6df806e7ff942680209fc3ab03a
gokuljs <gokuljs029@gmail.com>
2021-11-04 12:14:40 +0530

    subway

--- swiggy-subway/src/components/subway/subway.component.jsx
+++ swiggy-subway/src/components/subway/subway.component.jsx
@@ -3,14 +3,23 @@ import './subway.styles.scss';
 
 const Subway=()=>(
     <div class="container">
-        <div className="section-1">
+        <div className="section-1 section">
+            <div className="section-border">
+            <img className="subway-logo" src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/subway_020_ooxsox" />
+            </div>
+        </div>
+        <div className="section-2 section">
+        <h1>Subway</h1>
+        <div className="sub">
+        <p><span className="sub-1">Restaurant Unserviceable </span><span className="sub-2">Fast Food, Salads, Snacks, Desserts, Beverages</span></p> 
+        </div>
+        <div>
+        <p className="sub-3">Central Bangalore, Ashok Nagar | Change Outlet </p>
 
         </div>
-        <div className="section-2">
-
         </div>
-        <div className="section-3">
-
+        <div className="section-3 section">
+<p>kansakns</p>
         </div>
     </div>
 )
