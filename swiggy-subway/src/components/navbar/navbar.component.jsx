6bb827d9b2b343e9ba6d693a9add21d105e6803a
gokuljs <gokuljs029@gmail.com>
2021-11-04 16:23:10 +0530

    updating the subway content

--- swiggy-subway/src/components/navbar/navbar.component.jsx
+++ swiggy-subway/src/components/navbar/navbar.component.jsx
@@ -8,19 +8,19 @@ import './navbar.styles.scss';
         <a href="#" className="nav-logo"><img className="brand" src={swiggy}/></a>
         <ul className="nav-menu">
             <li className="nav-item">
-                <a href="#" className="nav-link"><i class="fas fa-search"></i>Search</a>
+                <a href="#" className="nav-link"><i class="fas fa-search test-1"></i>Search</a>
             </li>
             <li className="nav-item">
-                <a href="#" className="nav-link"><i class="fas fa-percentage"></i>offers</a>
+                <a href="#" className="nav-link"><i class="fas fa-percentage test-1"></i>offers</a>
             </li>
             <li className="nav-item">
-                <a href="#" className="nav-link"><i class="far fa-life-ring"></i>Help</a>
+                <a href="#" className="nav-link"><i class="far fa-life-ring test-1"></i>Help</a>
             </li>
             <li className="nav-item">
-                <a href="#" className="nav-link"><i class="far fa-user"></i>Sign In</a>
+                <a href="#" className="nav-link"><i class="far fa-user test-1"></i>Sign In</a>
             </li>
             <li className="nav-item">
-                <a href="#" className="nav-link"><i class="fas fa-cart-plus"></i>Cart</a>
+                <a href="#" className="nav-link"><i class="fas fa-cart-plus test-1"></i>Cart</a>
             </li>
         </ul>
         {/* <div className="hamburger">
--- swiggy-subway/src/components/subway/subway.component.jsx
+++ swiggy-subway/src/components/subway/subway.component.jsx
@@ -12,14 +12,48 @@ const Subway=()=>(
         <h1>Subway</h1>
         <div className="sub">
         <p><span className="sub-1">Restaurant Unserviceable </span><span className="sub-2">Fast Food, Salads, Snacks, Desserts, Beverages</span></p> 
-        </div>
-        <div>
         <p className="sub-3">Central Bangalore, Ashok Nagar | Change Outlet </p>
+        </div>
+        <div className="ratings">
+            <div className="row-container">
+
+            <div className="row-1">
+                <p><i class="fas fa-star"></i> 4.3</p>
+                <p className="row-1-subclass">100+ Ratings</p>
+            </div>
+            <div className="row-2">
+            <p>Unserviceable</p>
+            <p className="row-2-subclass">Check Back Later</p>
+            </div>
+            <div className="row-3">
+            <p><i class="fas fa-rupee-sign"></i> 350</p>
+            <p className="row-3-subclass">cost for two</p>
+    
+            </div>
+            </div>
 
         </div>
         </div>
         <div className="section-3 section">
-<p>kansakns</p>
+            <fieldset>
+                <legend>OFFER</legend>
+                <div className="offer">
+                    <div className="offer-1">
+                    <p className="content">
+                   <span> <i class="fas fa-percentage offer-logo"></i></span> 
+                   <span>20% off upto to 50% | use code SWIGGYIT</span>
+
+                    </p>
+                    </div>
+                    <div className="offer-2">
+                    <p className="content">
+                   <span> <i class="fas fa-percentage offer-logo"></i></span> 
+                   <span>Free delivery upto 30 on  orders above 149|use code FREEDEL</span>
+
+                    </p>
+                    </div>
+                </div>
+            </fieldset>
         </div>
     </div>
 )
