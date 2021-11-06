2aab5e34bdfe988215926513af10165238e389ac
gokuljs <gokuljs029@gmail.com>
2021-11-06 20:32:03 +0530

    working with search and list component

---
+++ swiggy-subway/src/components/list/list-component.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './list-styles.scss';
+
+class List extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            menulists:["BFF Combos","Classic Combo",
+                "Fusion Combo",
+                "Fusion Range",
+               " Meal for One",
+                "Sandwiches",
+                "Signature Wraps",
+                "Salads",
+                "Sides",
+                "Cookies and Desserts"],
+        }
+    }
+    render(){
+        return(
+            <div className="items">
+                <p class="header-1">Recommended</p>
+                {
+                    this.state.menulists.map(list=><p>{list}</p>)
+                }
+            </div>
+        )
+    }
+
+}
+export default List;
--- swiggy-subway/src/components/menu-card/menucard.component.jsx
+++ swiggy-subway/src/components/menu-card/menucard.component.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './menucard.styles.scss';
+import List from '../list/list-component';
 
-const MenuCard=()=>(
-    <div class="menu">
-        <p>hello</p>
-    </div>
-);
+class MenuCard extends React.Component{
+    render(){
+        return(
+            <div class="menu">
+                <div class="menu-1"><List/></div>
+                <div class="menu-2"> two </div>
+                <div class="menu-3">three</div>
+        
+            </div>
+        );
+    }
+}
+
 
 export default MenuCard;
