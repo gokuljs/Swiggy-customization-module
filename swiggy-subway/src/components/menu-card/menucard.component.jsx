c33a0188a7d417f1c851fa0e78b6cac5c4707822
gokuljs <gokuljs029@gmail.com>
2021-11-07 20:07:41 +0530

    menu-list menu-card working with props

--- swiggy-subway/src/components/menu-card/menucard.component.jsx
+++ swiggy-subway/src/components/menu-card/menucard.component.jsx
@@ -5,6 +5,69 @@ import MenuList from '../menu-list/menu-list.component';
 import SearchField from '../search/search.component';
 
 class MenuCard extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            items:[
+                {
+                    type:"veg",
+                    name:"BFF Veg Sub Combo (15 cm, 6 Inch)",
+                    price:"371",
+                    imgUrl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/ieqpaerjsv5zwbm2uxks",
+                    sub:"Buy any two 6 veg sub & get 2 cookie free",
+
+
+                },
+                {
+                    type:"veg",
+                    name:"BFF Veg Sub Combo (15 cm, 6 Inch)",
+                    price:"371",
+                    imgUrl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/ieqpaerjsv5zwbm2uxks",
+                    sub:"Buy any two 6 veg sub & get 2 cookie free",
+
+
+                },{
+                    type:"veg",
+                    name:"BFF Veg Sub Combo (15 cm, 6 Inch)",
+                    price:"371",
+                    imgUrl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/ieqpaerjsv5zwbm2uxks",
+                    sub:"Buy any two 6 veg sub & get 2 cookie free",
+
+
+                },{
+                    type:"veg",
+                    name:"BFF Veg Sub Combo (15 cm, 6 Inch)",
+                    price:"371",
+                    imgUrl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/ieqpaerjsv5zwbm2uxks",
+                    sub:"Buy any two 6 veg sub & get 2 cookie free",
+
+
+                },{
+                    type:"veg",
+                    name:"BFF Veg Sub Combo (15 cm, 6 Inch)",
+                    price:"371",
+                    imgUrl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/ieqpaerjsv5zwbm2uxks",
+                    sub:"Buy any two 6 veg sub & get 2 cookie free",
+
+
+                },{
+                    type:"veg",
+                    name:"BFF Veg Sub Combo (15 cm, 6 Inch)",
+                    price:"371",
+                    imgUrl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/ieqpaerjsv5zwbm2uxks",
+                    sub:"Buy any two 6 veg sub & get 2 cookie free",
+
+
+                },
+                
+
+            ],
+        }
+                
+            
+        
+    }
+    
     render(){
         return(
             <div class="menu">
@@ -12,7 +75,8 @@ class MenuCard extends React.Component{
                 <div class="menu-2"> 
                     <SearchField/>
                     <h1 className="menucard-header"> Recommended </h1>
-                    <MenuList/>
+        
+                    <MenuList items={this.state.items}/>
                 </div>
                 <div class="menu-3">three</div>
         
--- swiggy-subway/src/components/menu-list/menu-list.component.jsx
+++ swiggy-subway/src/components/menu-list/menu-list.component.jsx
@@ -2,33 +2,35 @@ import React from "react";
 import veg from './veg.png';
 import './menu-list.styles.scss';
 
-class MenuList extends React.Component{
-    constructor(){
-        super();
-        this.state={
-            menulist:[],
-        }
-    }
 
-    render(){
-    return(
+
+
+const MenuList=props=>(
+        <div>
+        {
+            props.items.map(item=>(
+                <div className="menulist">
+
+            
+                {/* <h3> Recommended </h3> */}
+                <div className="menulist-1">
+                    {/* <p>{type}</p> */}
+                    <img src={veg} className="menulist-img"/>
+                    <h4 className="menulist-header">{item.name}</h4>
+                    <p className="menulist-price"><i class="fas fa-rupee-sign" id="ruppes"></i>{item.price}</p>
+                    <p className="menulist-desc">{item.sub}</p>
+                </div>
+                <div className="menulist-2">
+                    <img className="menulist-image" src={item.imgUrl}/>
+                    <button className="menulist-button">Add</button>
+                </div>
         
-        <div className="menulist">
-        {/* <h3> Recommended </h3> */}
-        <div className="menulist-1">
-            <img src={veg} className="menulist-img"/>
-            <h4 className="menulist-header">BFF Veg Sub Combo (15 cm, 6 Inch)</h4>
-            <p className="menulist-price"><i class="fas fa-rupee-sign" id="ruppes"></i>341</p>
-            <p className="menulist-desc">Buy any two signature wrap & get 2 cookie free</p>
+        
+                </div>
+            ))
+        }
         </div>
-        <div className="menulist-2">
-            <img className="menulist-image" src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/co52e1edcdv2ftwvyhcp"/>
-        </div>
+    );
 
-
-        </div>
-    )
-    }
-}
 
 export default MenuList;
